74b0bfa7693b129c1b78193af9e7cdcee50e618d
nightzsky <33797097+nightzsky@users.noreply.github.com>
2018-03-13 21:42:12 +0800

    hi

--- backendserver.py
+++ backendserver.py
@@ -91,7 +91,8 @@ def register_kyc():
     #    print("block id: %d"%block.id)
     block_id = hash256(user_info["id_number"])
     r=requests.post("http://173.193.102.98:31090/api/User", data = {"$class": "org.acme.biznet.User","userId": block_id,"name": encrypted_user_info["name"],"userData": {"$class": "org.acme.biznet.UserData","name": encrypted_user_info["name"],"id": encrypted_user_info["id_number"],"postcode": encrypted_user_info["postal_code"],"birthdate": encrypted_user_info["dob"]},"access": True})
-    print r.status_code  
+    print r.status_code
+    print r.text
     #store private key, AES key, and user's block id in the token
     #first get private key as plaintext
     f = open("privateKey.pem", "a+b")
