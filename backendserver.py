6fef3e849b476b647f4db3d8bbf2265faf6d7193
nightzsky <33797097+nightzsky@users.noreply.github.com>
2018-03-14 21:11:33 +0800

    hi

--- backendserver.py
+++ backendserver.py
@@ -24,6 +24,7 @@ import json
 import string
 import requests
 import os
+import ast
 from functools import wraps
 app = Flask(__name__)
 
@@ -181,124 +182,124 @@ def register_org():
  
 #function which encrypts data using AES
 def aes_encrypt(data,key):
-	#process data to become suitable for encryption by converting to bytes if needed
-	if type(data) != bytes:
-		data = bytes(data, encoding = "utf8")
-	iv = Random.new().read(AES.block_size)
-	cipher = AES.new(key, AES.MODE_CFB,iv)
-	return str(list((iv+cipher.encrypt(data))))
+    #process data to become suitable for encryption by converting to bytes if needed
+    if type(data) != bytes:
+        data = bytes(data, encoding = "utf8")
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CFB,iv)
+    return str(list((iv+cipher.encrypt(data))))
 
 #function which decrypts data using AES
 def aes_decrypt(data,key):
-	if type(data) != bytes:
-		try:
+    if type(data) != bytes:
+        try:
             print(data)
-			data = bytes(ast.literal_eval(data))
-		except:
-			print("Error: could not interpret data for decryption")
-			return
-	iv = data[:16]
-	cipher = AES.new(key, AES.MODE_CFB, iv)
-	decrypted = cipher.decrypt(data[16:]).decode()
-	return decrypted
+            data = bytes(ast.literal_eval(data))
+        except:
+            print("Error: could not interpret data for decryption")
+            return
+    iv = data[:16]
+    cipher = AES.new(key, AES.MODE_CFB, iv)
+    decrypted = cipher.decrypt(data[16:]).decode()
+    return decrypted
 
 #function which encrypts data using RSA
 def rsa_encrypt(data, public_key):
-	if type(data) != bytes:
-		data = bytes(data, encoding = "utf8")
-	cipher = PKCS1_OAEP.new(public_key)
-	#hybrid encryption is used here as PKCS1_OAEP can only encrypt a very small amount of data
-	#to get around this, AES is used for the encryption of the data, and the AES key is encrypted using RSA
-	session_key = Random.get_random_bytes(16)
-	encrypted_data = aes_encrypt(data,session_key)
-	encrypted_session_key = cipher.encrypt(session_key)
-	return [encrypted_data, encrypted_session_key]
+    if type(data) != bytes:
+        data = bytes(data, encoding = "utf8")
+    cipher = PKCS1_OAEP.new(public_key)
+    #hybrid encryption is used here as PKCS1_OAEP can only encrypt a very small amount of data
+    #to get around this, AES is used for the encryption of the data, and the AES key is encrypted using RSA
+    session_key = Random.get_random_bytes(16)
+    encrypted_data = aes_encrypt(data,session_key)
+    encrypted_session_key = cipher.encrypt(session_key)
+    return [encrypted_data, encrypted_session_key]
 
 #function which decrypts data using RSA
 def rsa_decrypt(data, private_key):
-	cipher = PKCS1_OAEP.new(private_key)
-	#first decrypt the session key using RSA
-	session_key = cipher.decrypt(data[1])
-	#then decrypt the data using AES and the session key
-	return aes_decrypt(data[0], session_key)
+    cipher = PKCS1_OAEP.new(private_key)
+    #first decrypt the session key using RSA
+    session_key = cipher.decrypt(data[1])
+    #then decrypt the data using AES and the session key
+    return aes_decrypt(data[0], session_key)
 
 
 #computes a SHA256 hash of the data
 def hash256(data):
-	data = bytes(data, encoding = "utf8")
-	hash_object = SHA256.new(data=data)
-	return hash_object.hexdigest()
+    data = bytes(data, encoding = "utf8")
+    hash_object = SHA256.new(data=data)
+    return hash_object.hexdigest()
 
 #function that returns the root hash of the merkle tree from a list of data
 #it assumes that everything in the data has already been hashed
 def merkle(data):
-	#base case, only one data point, return that one data point
-	if len(data) == 1:
-		return data[0]
+    #base case, only one data point, return that one data point
+    if len(data) == 1:
+        return data[0]
 
-	#another base case, compute hash of the concatenation of the 2 leaves
-	if len(data) == 2:
-		return hash256(data[0] + data[1])
+    #another base case, compute hash of the concatenation of the 2 leaves
+    if len(data) == 2:
+        return hash256(data[0] + data[1])
 
-	#if odd number, ignore last data point  
-	if (len(data)%2 != 0):
-		data = data[:-1]
+    #if odd number, ignore last data point  
+    if (len(data)%2 != 0):
+        data = data[:-1]
 
-	#recursively traverse the merkle tree from bottom to top to get the root hash
-	temp = []
-	for i in range(0,len(data),2):
-		temp.append(merkle(data[i:i+2]))
+    #recursively traverse the merkle tree from bottom to top to get the root hash
+    temp = []
+    for i in range(0,len(data),2):
+        temp.append(merkle(data[i:i+2]))
 
-	return merkle(temp)
+    return merkle(temp)
 
 def isValidName(name):
-	if type(name)!=str:
-		return False
-	for letter in name:
-		if letter.isdigit() or letter in string.punctuation:
-			return False
-	return True
+    if type(name)!=str:
+        return False
+    for letter in name:
+        if letter.isdigit() or letter in string.punctuation:
+            return False
+    return True
 
 def isValidId(idee):
-	if type(idee) != str:
-		return False
-	for letter in idee:
-		if not letter.isalnum():
-			return False
-	return True
+    if type(idee) != str:
+        return False
+    for letter in idee:
+        if not letter.isalnum():
+            return False
+    return True
 
 def isValidDob(dob):
-	split = dob.split("/")
-	if len(split) != 3:
-		return False
-	for num in split:
-		if not num.isdigit():
-			return False
-	if len(split[0]) != 2 or len(split[1]) != 2 or len(split[2])!= 4:
-		return False
+    split = dob.split("/")
+    if len(split) != 3:
+        return False
+    for num in split:
+        if not num.isdigit():
+            return False
+    if len(split[0]) != 2 or len(split[1]) != 2 or len(split[2])!= 4:
+        return False
 
-	if int(split[1]) > 12 or int(split[1]) == 0:
-		return False
+    if int(split[1]) > 12 or int(split[1]) == 0:
+        return False
 
-	long_months = ['01', '03', '05', '07', '08', '10', '12']
-	short_months = ['04', '06', '09', '11']
+    long_months = ['01', '03', '05', '07', '08', '10', '12']
+    short_months = ['04', '06', '09', '11']
 
 
-	if split[1] == '02' and int(split[0]) > 29:
-		return False
-	elif split[1] in long_months and int(split[0]) > 31:
-		return False
-	elif split[1] in short_months and int(split[0]) > 31:
-		return False
+    if split[1] == '02' and int(split[0]) > 29:
+        return False
+    elif split[1] in long_months and int(split[0]) > 31:
+        return False
+    elif split[1] in short_months and int(split[0]) > 31:
+        return False
 
-	return True
+    return True
 
 def isValidPostCode(postcode):
-	if len(postcode) != 6:
-		return False
-	if not postcode.isdigit():
-		return False
-	return True
+    if len(postcode) != 6:
+        return False
+    if not postcode.isdigit():
+        return False
+    return True
 
 
 
