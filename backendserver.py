680aba9e46b62d79a130c6c83b0e88b26b50b60b
nightzsky <33797097+nightzsky@users.noreply.github.com>
2018-03-22 22:54:34 +0800

    updated kyc

--- backendserver.py
+++ backendserver.py
@@ -133,17 +133,7 @@ def register_kyc():
     postal_code = decrypted["postal_code"]
     id_number = decrypted["id_number"]
     dob = decrypted["dob"]
-#    decrypted = {}
-#    name = request.json["name"]
-#    postal_code = request.json["postal_code"]
-#    id_number = request.json["id_number"]
-#    dob = request.json["dob"]
-    
-#    decrypted["name"]= name
-#    decrypted["postal_code"] = postal_code
-#    decrypted["id_number"] = id_number
-#    decrypted["dob"] = dob
-    #check if the info is valid
+
     if not isValidInput(decrypted):
         resp = Response(json.dumps({"message":"invalid input"}))
         resp.status_code = 400
