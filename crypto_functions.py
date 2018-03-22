d3af55f90415090b2a4abeabb4b09fc843c643c2
nightzsky <33797097+nightzsky@users.noreply.github.com>
2018-03-23 00:09:58 +0800

    debugging

--- crypto_functions.py
+++ crypto_functions.py
@@ -56,7 +56,7 @@ def rsa_decrypt(data, private_key):
     cipher = PKCS1_OAEP.new(private_key)
     #first decrypt the session key using RSA
     if type(data[1] != bytes):
-        data[1] = bytes(data[1])
+        data[1] = bytes(java_to_python_bytes(data[1]))
     print("encrypted session key: %s"%list(data[1]))
     session_key = cipher.decrypt(java_to_python_bytes(data[1]))
     print("session key: %s"%list(session_key))
