b040be55d8bc53460ee358cfb8e5285b4eb9a202
nightzsky <33797097+nightzsky@users.noreply.github.com>
2018-03-22 23:54:26 +0800

    debugging

--- backendserver.py
+++ backendserver.py
@@ -121,7 +121,7 @@ def decrypt_request(json):
 #        if type(json[key]) == dict:
 #            decrypted[rsa_decrypt(java_to_python_bytes(key),private_key)] = decrypt_request(json[key])
 #        else:
-        decrypted[rsa_decrypt(java_to_python_bytes(key),private_key)] = rsa_decrypt(java_to_python_bytes(json[key]), private_key)
+        decrypted[rsa_decrypt(key,private_key)] = rsa_decrypt(json[key], private_key)
 
     return decrypted
 
